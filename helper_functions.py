2ec9c7121bea5798701583ce985004ad86eb96d0
Tom Maisey <twm@twm-mbp.local>
2025-05-03 00:46:08 +0100

    scales fig height with 2x n_rows

--- helper_functions.py
+++ helper_functions.py
@@ -171,7 +171,7 @@ def show_batch_images(
     # initiate figure setup
     n_rows = n_images // images_per_row
     fig, axs = plt.subplots(
-        n_rows, images_per_row, figsize=(15, 5 * n_rows), sharex=False
+        n_rows, images_per_row, figsize=(16, 2 * n_rows), sharex=False
     )
     axs = axs.flatten()
 
